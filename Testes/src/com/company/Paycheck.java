764497651b686647464e8eb3fafcd5b9538f90a0
Jr <vsj@ic.ufal.br>
2017-07-31 10:47:01 -0300

    Tipo de Agenda Feita Aceita Pelo Professor.
    Resta apenas Implementar a Função 7 - REDO - UNDO.
    *Arquivo esquecido Incluido

---
+++ Testes/src/com/company/Paycheck.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Paycheck {
+
+    private Employee employee;
+    private float money;
+
+    public Paycheck(Employee employee, float money) {
+
+        this.employee = employee;
+        this.money = money;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+}
